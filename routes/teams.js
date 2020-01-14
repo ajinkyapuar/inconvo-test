84722daeed9fc6b3ad714c664aa42bb8778f8746
Ajinkya Puar <puar.ajinkya@gmail.com>
2020-01-14 16:06:38 +0530

    Teams Create / Update Completed

--- routes/teams.js
+++ routes/teams.js
@@ -1,15 +1,19 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const teamsRoutes = (app, fs) => {
     // variables
     const dataPath = './data/football.json';
 
-    // READ
+    // READ Teams
     app.get('/teams', (req, res) => {
         fs.readFile(dataPath, 'utf8', (err, data) => {
             if (err) {
                 throw err;
             }
-            console.log(data);
-            res.send(JSON.parse(data));
+            let dataArr = JSON.parse(data);
+            res.send(dataArr);
         });
     });
 
@@ -19,18 +23,58 @@ const teamsRoutes = (app, fs) => {
             if (err) {
                 throw err;
             }
-            // console.log(data);
-            // console.log(req.params["name"]);
 
-            var result = JSON.parse(data).filter(obj => {
-                return obj.name === req.params["name"];
+            let dataArr = JSON.parse(data);
+
+            let teamName = capitalizeFirstLetter(req.body["name"]);
+
+            var result = dataArr.filter(obj => {
+                return obj["name"] === teamName;
               })
-            console.log(result);
+
             res.send(result);
         });
     });
 
+    // Create or Update Team
+    app.post('/teams', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
 
+            let dataArr = JSON.parse(data);
+            
+            let teamName = capitalizeFirstLetter(req.body["name"]);
+
+            let teamObj = dataArr.find(obj => obj.name === teamName);
+            
+            let teamIdx = dataArr.findIndex(obj => obj.name === teamName);
+            
+            let Idx;
+
+            if(teamObj){
+                // Team exists
+                dataArr[teamIdx]["img"] = req.body["img"];
+                idx = teamIdx
+            }
+            else{
+                // Team does not exists
+                let newTeam = req.body;
+                newTeam["name"] = capitalizeFirstLetter(newTeam["name"]) 
+                dataArr.push(newTeam);
+                idx = dataArr.length - 1
+            }
+
+            fs.writeFile(dataPath, JSON.stringify(dataArr), 'utf8', (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+            
+            return res.status(200).send();;
+        });
+    });
 };
 
 module.exports = teamsRoutes;
